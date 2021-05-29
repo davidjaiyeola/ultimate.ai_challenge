ccca377edf8233ec2dae5f97ebab55546296575e
David Jaiyeola <mannasoft.rd@gmail.com>
2021-05-29 14:31:08 +0100

    added swagger

--- src/main/java/com/ultimate/ai/challenge/controller/SingleReplyController.java
+++ src/main/java/com/ultimate/ai/challenge/controller/SingleReplyController.java
@@ -3,6 +3,8 @@ package com.ultimate.ai.challenge.controller;
 import com.ultimate.ai.challenge.dto.IntentApiRequestDto;
 import com.ultimate.ai.challenge.dto.SingleReplyResponseDto;
 import com.ultimate.ai.challenge.service.SingleReplyService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController("/api/v1")
+@Api(value = "ChatBot", description = "Rest API for ChatBot operations", tags = "ChatBot API")
 public class SingleReplyController {
     private SingleReplyService singleReplyService;
 
@@ -17,6 +20,7 @@ public class SingleReplyController {
         this.singleReplyService = singleReplyService;
     }
 
+    @ApiOperation(value = "Reply", response = SingleReplyResponseDto.class, produces = "application/json")
     @PostMapping( value="/reply", produces ="application/json")
     public Mono<ResponseEntity<SingleReplyResponseDto>> reply(@RequestBody IntentApiRequestDto dto) {
         return singleReplyService.singleReply(dto).map(intent -> ResponseEntity.ok().body(new SingleReplyResponseDto(intent)));
--- src/main/java/com/ultimate/ai/challenge/dto/IntentApiRequestDto.java
+++ src/main/java/com/ultimate/ai/challenge/dto/IntentApiRequestDto.java
@@ -2,9 +2,11 @@ package com.ultimate.ai.challenge.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class IntentApiRequestDto {
     private String botId;
     private String message;
